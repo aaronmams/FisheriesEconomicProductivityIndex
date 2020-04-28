441d3c1979fa7a643da02b333cc60cd71a6737aa
emilyhmarkowitz <emilymarkowitz@gmail.com>
2020-04-28 17:24:35 -0400

    Testing Package 3

--- R/DocumentMetadata.R
+++ R/DocumentMetadata.R
@@ -2,9 +2,10 @@
 library(officer)
 
 
-#' Document Metadata
-#'
-#' A function to document your metadata for people who hate code things but insist they want to know what you are doing
+#' @title Document Metadata
+#' 
+#' @source 
+#' @description A function to document your metadata for people who hate code things but insist they want to know what you are doing
 #' @param dir.out The directory that you want this file to be saved to. 
 #' @param title Name of the project, to appear at the top of the page. 
 #' @author Emily Markowitz Emily.Markowitz@noaa.gov
